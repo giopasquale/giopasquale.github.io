56ccc389fb0aaaa0aada99aaae21391365957657
giopasquale <32724291+giopasquale@users.noreply.github.com>
2017-10-13 09:03:05 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,18 @@
+<?
+
+// Coloque a mensagem que irá ser enviada para
+seu e-mail abaixo:
+$msg = "Mensagem enviada em ".date("d/m/Y").",
+os dados seguem abaixo:
+";
+while(list($campo, $valor) = each($HTTP_POST_VARS))
+{
+  $msg .= ucwords($campo).": ".$valor."
+";
+}
+// Agora iremos fazer com que o PHP envie os dados do
+Formulário para seu e-mail: 
+mail("gioopasquale@gmail.com", "Assunto do
+E-mail",$msg,"From: $REMOTE_ADDR");
+echo "Seu e-mail foi enviado com sucesso. Obrigado";
+?>
