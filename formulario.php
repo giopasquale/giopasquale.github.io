badf06564810fa3d465fd75b097ce5d98b7a0e7c
giopasquale <32724291+giopasquale@users.noreply.github.com>
2017-10-14 02:04:21 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,18 +1,29 @@
-<?
-
-// Coloque a mensagem que irá ser enviada para
-seu e-mail abaixo:
-$msg = "Mensagem enviada em ".date("d/m/Y").",
-os dados seguem abaixo:
-";
-while(list($campo, $valor) = each($HTTP_POST_VARS))
-{
-  $msg .= ucwords($campo).": ".$valor."
-";
-}
-// Agora iremos fazer com que o PHP envie os dados do
-Formulário para seu e-mail: 
-mail("gioopasquale@gmail.com.br", "Assunto do
-E-mail",$msg,"From: $REMOTE_ADDR");
-echo "Seu e-mail foi enviado com sucesso. Obrigado";
+<?php
+ 
+/*iremos declarar as variaveis que recebemos pelo método post
+ 
+lembra lá na explicação do metodo post o que deveria ser feito? então, os dados
+ 
+serão pegos por aqui e transformados em comunicação entre server e cliente para gerar
+ 
+o email do cara e te enviar*/
+ 
+$nome=$_POST[nome];//aqui pega os dados que foram preenchidos la no formulário com o ID NOME
+$email=$_POST[email];//aqui a mesma coisa, mas com o email
+$assunto=$_POST[assunto];//aqui a mesma coisa, mas com o assunto
+$mensagem=$_POST[mensagem];//aqui a mesma coisa, mas com o assunto
+ 
+//agora vamos enviar todos esses dados usando a função mail que é do PHP
+mail("meuemail@meudominio.com.br","$assunto","
+Nome: $nome
+Email: $email
+Assunto: $assunto
+Mensagem: $mensagem","FROM:$nome<$email>");
+ 
+/*Ele diz assim pro código: "Envia um email para meuemail@meudominio.com.br e que esse email tenha os dados que
+foram pegos em ASSUNTO, NOME, EMAIL, ASSUNTO e MENSAGEM, eles foram pegos com o MÉTODO POST e em FROM vai conter
+os dados de quem enviou o email, ou seja, la na caixa de entrada do teu e-mail vai ter isso. :)*/
+ 
+echo "sua mensagem foi enviada com sucesso!"; //aí mostramos no navegador da pessoa que enviou o email uma mensagem de sucesso
+ 
 ?>
